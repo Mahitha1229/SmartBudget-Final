4e8a417c0c3d89f4424117ed2ae4a403277d38bd
Mahitha Upadrasta <mahithaupadrasta@gmail.com>
2026-08-13 23:14:40 +0530

    refactor: use centralized theme store for color scheme

--- components/parallax-scroll-view.tsx
+++ components/parallax-scroll-view.tsx
@@ -8,7 +8,6 @@ import Animated, {
 } from 'react-native-reanimated';
 
 import { ThemedView } from '@/components/themed-view';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 import { useThemeColor } from '@/hooks/use-theme-color';
 
 const HEADER_HEIGHT = 250;
